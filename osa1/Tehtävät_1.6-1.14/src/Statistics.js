d2a4263723374f8f50c4d50b02016ab1e5a904e2
EXT-Janne.Suontausta <EXT-Janne.Suontausta@terveystalo.com>
2022-06-10 13:50:56 +0300

    Tehtävä 1.9: unicafe step4

--- osa1/Tehtävät_1.6-1.14/src/Statistics.js
+++ osa1/Tehtävät_1.6-1.14/src/Statistics.js
@@ -2,6 +2,15 @@ const Statistics = (props) => {
     let all = props.good + props.neutral + props.bad;
     let average = (props.good * 1 + props.neutral * 0 - props.bad * 1) / all;
     let positive = all > 0 ? props.good / all * 100 : 0;
+
+    if (all === 0) {
+        return (
+            <div>
+                <p>No feedback given</p>
+            </div>
+        )
+    }
+
     return (
       <div>
         <p>good {props.good}</p>
