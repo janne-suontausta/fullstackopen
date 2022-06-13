225fc73023f10d8a84156f93d4d5fa1c87b785c4
EXT-Janne.Suontausta <EXT-Janne.Suontausta@terveystalo.com>
2022-06-13 12:42:11 +0300

    Tehtävä 2.11: puhelinluettelo step6

--- osa2/Tehtävät_2.6-2.11/src/components/App.js
+++ osa2/Tehtävät_2.6-2.11/src/components/App.js
@@ -1,20 +1,25 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from "./Filter"
 import Persons from "./Persons";
 import PersonForm from "./PersonForm";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filteredPersons, setFilteredPersons] = useState(persons);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [nameFilter, setNameFilter] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+        setFilteredPersons(response.data);
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
